4354b4bd691f76825726166d72f47bdfa3214be6
James <jmsn311@gmail.com>
2024-06-04 09:21:41 -0600

    completed lab

---
+++ index.js
@@ -0,0 +1,6 @@
+function divide (parameter1, parameter2)  {
+    return parameter1 / parameter2
+}
+const square = (parameter1) => parameter1 * parameter1;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2
